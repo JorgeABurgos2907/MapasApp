7087250cbec920e781cb4a139fc3ba7dfa758dd6
Jorge Burgos <burgosjorge2907@gmail.com>
2021-12-27 11:48:51 -0500

    Zoom Range

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as mapboxgl from 'mapbox-gl';
+
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+
+    `
+    .mapa-container {
+      width: 100%;
+      height: 100%
+    }  
+
+    .row{
+      background-color: white;
+      bottom: 50px;
+      left: 50px;
+      padding: 10px;
+      border-radius: 5px;
+      position: fixed;
+      z-index: 999;
+    }
+  `
+
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +31,16 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    // (mapboxgl as any).accessToken = environment.mapboxToken;
+    
+    var map = new mapboxgl.Map({
+    container: 'mapa',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [ -73.1237818, 7.099439227372295],
+    zoom: 16
+});
+
   }
 
 }
