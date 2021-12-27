40755d44d094168932321fa27e3e167265ba6a44
Jorge Burgos <burgosjorge2907@gmail.com>
2021-12-27 11:23:47 -0500

    Mapa pantalla completa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1Ijoiam9yZ2VhYnVyZ29zIiwiYSI6ImNreGM4d21jeDB1bmQybnN0N3FyYndxenQifQ.sTvQe7lFOYQemB1VnRVrHQ'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1Ijoiam9yZ2VhYnVyZ29zIiwiYSI6ImNreGM4d21jeDB1bmQybnN0N3FyYndxenQifQ.sTvQe7lFOYQemB1VnRVrHQ'
 };
 
 /*
